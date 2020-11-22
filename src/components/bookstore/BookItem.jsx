d637d6b0c96787616a53d89248030e0892dbbf93
Amir Bastidas <53383690+amirbv@users.noreply.github.com>
2020-11-22 15:51:36 -0400

    remove snipcart

--- src/components/bookstore/BookItem.jsx
+++ src/components/bookstore/BookItem.jsx
@@ -1,53 +1,46 @@
 import React from "react";
 import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@material-ui/core";
-// import { useRouteMatch } from "react-router-dom";
 import { amber } from "@material-ui/core/colors";
 
 
 
 export default function BookItem({ book }) {
-  // const { path } = useRouteMatch();
 
   return (
-    <Card className="book-card">
-      <div className="book-card-image">
-        <CardMedia
-          className="book-image"
-          component="img"
-          image={book.imageURL}
-        />
-        <div className="absolute-description">
-          <Typography variant="caption">
-            {book.description}
-          </Typography>
-          <Typography variant="caption">Editorial: {book.publisher}</Typography>
+    <>
+      <Card className="book-card">
+        <div className="book-card-image">
+          <CardMedia
+            className="book-image"
+            component="img"
+            image={book.imageURL}
+            alt={book.title + ' preview'}
+          />
+          <div className="absolute-description">
+            <Typography variant="caption">
+              {book.description}
+            </Typography>
+            <Typography variant="caption">Editorial: {book.publisher}</Typography>
+          </div>
         </div>
-      </div>
-      <CardContent>
-        <Typography component="h3" variant="h5" noWrap>
-          {book.title}
-        </Typography>
-        <Typography component="p" variant="caption" noWrap>
-          {book.author}
-        </Typography>
-        <Typography component="p" variant="subtitle2" >
-          ${book.price}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button
-          className="snipcart-add-item"
-          data-item-id={book._id}
-          data-item-image={book.imageURL}
-          data-item-name={book.title}
-          data-item-url="/"
-          data-item-price={book.price}
-          variant="contained"
-          style={{background: amber[500]}}
-        >
-          Comprar
-        </Button>
-      </CardActions>
-    </Card>
+        <CardContent>
+          <Typography component="h3" variant="h5" noWrap>
+            {book.title}
+          </Typography>
+          <Typography component="p" variant="caption" noWrap>
+            {book.author}
+          </Typography>
+          <Typography component="p" variant="subtitle2" >
+            ${book.price}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" style={{background: amber[500]}}>
+            Comprar
+          </Button>
+        </CardActions>
+      </Card>
+    
+    </>
   );
 }
