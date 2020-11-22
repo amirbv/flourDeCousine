a9ec6b57a954f071ed6700ef367b029139b2a03f
Amir Bastidas <53383690+amirbv@users.noreply.github.com>
2020-11-22 14:02:33 -0400

    Update PostInfo.jsx

--- src/components/post/PostInfo.jsx
+++ src/components/post/PostInfo.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Card, CardContent, Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
 import BackLink from '../global/BackLink';
 
 export default function PostInfo({ post }) {
